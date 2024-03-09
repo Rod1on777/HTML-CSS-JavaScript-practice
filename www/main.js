7e96232b400169ba506eaf682177603fec363bea
Rod1on777 <rodionk533@gmail.com>
2024-03-09 18:02:31 +1100

    function Demo1

--- www/main.js
+++ www/main.js
@@ -1,8 +1,6 @@
 // ------------------------- База ------------------------
 document.write('Hello World');  //Написать текст после тега script
 
-document.innerHTML = 'Hi..';
-
 console.log('Hello console');   // Вывод текста в консоль
         
 console.error('(not) Error')    //Вывод текста стилезованного под ошибку
@@ -11,7 +9,8 @@ console.warn('(not) warning!')  //Выод текста в формате пре
 
 
 //  ----------------------- Переменные -------------------------
-var num = 5;
+var num = 5;    //глобальная переменная
+let numb = 5;   //локальная переменная
 const con = 6;
 
 console.log(num);
@@ -19,4 +18,9 @@ console.log(num);
 console.log("Переменная= " + num + ".");   //Объединение вывода значений
 console.log("Константа= " + con + ".");
 
-document.getElementById("demo").innerHTML = '10';  //Берёт элемент с айди demo и меняет его на 10;
+document.getElementById("demo").innerHTML = '10';  //Берёт элемент с айди demo и меняет его на 10 (ИСПОЛЬЗОВАТЬ ПРЕДПОЧТИТЕЛЬНЕЕ ЧЕМ .write)
+
+function Demo1(){                                           //Простейшай функция Demo1 (заменяет элемент с айди demo1 на Demo1)
+    document.getElementById("demo1").innerHTML = "Demo1";
+}
+
