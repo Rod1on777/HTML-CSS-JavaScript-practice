e18869f43a50cff97ad7f7e28e1365f0cd1bdae6
Rod1on777 <rodionk533@gmail.com>
2024-04-12 15:55:11 +1000

    More custom elements

--- LitElemants/main.js
+++ LitElemants/main.js
@@ -8,17 +8,30 @@ content += '</ul>';
 //target.innerHTML = content;
 
 class ThingList extends HTMLElement {       //Кастомный элемент (это класс)
+
+    constructor() {                         // Пустой конструктор элемента
+        super();                            // хз чо
+        this.color = this.getAttribute('color') || 'purple';    // присваивает этому элементу значение color, если этого значения нет, то цвет меняется на фиолетовый
+        this.things = this.getAttribute('things') || ''         // присваивает этому элементу значение things
+    }
+
+
+
+
+
     connectedCallback() {
         console.log('I am connected');
 
-        const things = ['eggs', 'chease', 'biscuits', 'rice'];
+        const things = this.things.split(',');  // в переменную things записывается значие things из конструктора. Метод .split разделяет строку по каждой запятой
        
         let content = '<ul>';
         for (let index = 0; index < things.length; index++){
             content += `<li>${things[index]}</li>`;
         }
         content += '</ul>';
-        this.innerHTML = content;           //эта нада не знаю зачем
+        this.innerHTML = content;           //запись в HTML код
+
+        this.style =`color: ${this.color}`         // добавление стиля + цвета указанного в конструкторе
     }
 }
 
